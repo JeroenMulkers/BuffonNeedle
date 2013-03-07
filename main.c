5c56d6883f0b57bdcfa03201067c7031bfac2ef2
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-03-07 17:37:14 +0100

    Implementation of the algorithm

--- main.c
+++ main.c
@@ -4,8 +4,30 @@
  */
 
 #include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+const double PI = 3.14159265359;
 
 int main(int argc, const char *argv[]){
+
+    double length=0.8;
+    int N = 10000;
+
+    int Nhit = 0;
+    for(int i=0; i<N; i++) {
+
+        double a = 2*PI*rand()/RAND_MAX; // angle
+        double x = (double)rand()/RAND_MAX - 0.5; // middle
+
+        double x1 = x - (length/2)*cos(a);
+        double x2 = x + (length/2)*cos(a);
+
+        if(x1*x2 <= 0){Nhit++;}
+    }
+
+    double pi = 2 * length * N/Nhit;
+    printf("%f",pi);
 
     return 0;
 }
